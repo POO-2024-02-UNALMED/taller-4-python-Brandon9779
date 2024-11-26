d15791d2cd9217f848928d6e43db7d5a14e9a88f
Brandon9779 <elbrandon9779@gmail.com>
2024-11-25 23:40:10 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -12,31 +12,25 @@ class Grupo:
         for x in kwargs.values():
             self._asignaturas.append(Asignatura(x, None))
 
-    def agregarAlumno(self, alumno, *arg):
+    def agregarAlumno(self, alumno, *args):
         if isinstance(alumno, str):
             self.listadoAlumnos.append(alumno)
-        if arg:
-            for estudiante in arg:
-                if isinstance(estudiante, str):
-                    self.listadoAlumnos.append(estudiante)
-                elif isinstance(estudiante, list):
-                    self.listadoAlumnos.extend(estudiante)
+        for extra_alumnos in args:
+            if isinstance(extra_alumnos, list):
+                self.listadoAlumnos.extend(extra_alumnos)
 
-    def agregarGrupo(self, grupo, asignaturas=None, alumnos=None, *arg):
+    def agregarGrupo(self, grupo, asignaturas=None, alumnos=None, *args):
         self._grupo = grupo
         if asignaturas:
-           self._asignaturas.extend(asignaturas)
+            self._asignaturas.extend(asignaturas)
         if alumnos:
-           if isinstance(alumnos, list):
-               self.listadoAlumnos.extend(alumnos)
-           elif isinstance(alumnos, str):
-               self.listadoAlumnos.append(alumnos)
-        for extra_alumnos in arg:
+            if isinstance(alumnos, list):
+                self.listadoAlumnos.extend(alumnos)
+            elif isinstance(alumnos, str):
+                self.listadoAlumnos.append(alumnos)
+        for extra_alumnos in args:
             if isinstance(extra_alumnos, list):
-               self.listadoAlumnos.extend(extra_alumnos)
-            elif isinstance(extra_alumnos, str):
-               self.listadoAlumnos.append(extra_alumnos)
-
+                self.listadoAlumnos.extend(extra_alumnos)
 
     @classmethod
     def asignarNombre(cls, nombre="Grado 6"):
@@ -44,3 +38,4 @@ class Grupo:
 
     def __str__(self):
         return f"Grupo de estudiantes: {self._grupo}"
+
