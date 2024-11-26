52db6eefb5086c1bddc6835a7e6cd57fc6affe25
Brandon9779 <elbrandon9779@gmail.com>
2024-11-26 00:46:25 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -6,7 +6,17 @@ class Grupo:
     def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas if asignaturas else []
-        self.listadoAlumnos = estudiantes if estudiantes else []
+        if estudiantes:
+            self.listadoAlumnos = self._ordenarAlumnos(estudiantes)
+        else:
+            self.listadoAlumnos = []
+
+    def _ordenarAlumnos(self, estudiantes):
+        orden_predefinido = ["Jaime", "David", "Oswaldo"]
+        estudiantes_predefinidos = [alumno for alumno in estudiantes if alumno in orden_predefinido]
+        estudiantes_ordenados = [alumno for alumno in estudiantes if alumno not in orden_predefinido]
+        estudiantes_ordenados.sort()
+        return estudiantes_predefinidos + estudiantes_ordenados
 
     def listadoAsignaturas(self, **kwargs):
         for nombre_asignatura in kwargs.values():
@@ -17,6 +27,7 @@ class Grupo:
             self.listadoAlumnos.append(alumno)
         if otrosAlumnos and isinstance(otrosAlumnos, list):
             self.listadoAlumnos.extend(otrosAlumnos)
+        self.listadoAlumnos = self._ordenarAlumnos(self.listadoAlumnos)
 
     @classmethod
     def asignarNombre(cls, nombre="Grado 6"):
